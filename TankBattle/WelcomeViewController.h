d614ce9270b9024e3c1a60622343ebfcae59f8e6
CoderJay <396736694@qq.com>
2017-07-21 11:37:00 +0800

    Initial commit

---
+++ TankBattle/WelcomeViewController.h
@@ -0,0 +1,16 @@
+//
+//  WelcomeViewController.h
+//  TankWar1
+//
+//  Created by VanJay on 15/10/8.
+//  Copyright © 2015年 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class ViewController;
+
+@interface WelcomeViewController : UIViewController
+
+@property (weak, nonatomic) ViewController *viewController;
+
+@end
